10eba9d92e43378f363044eee8d5e144020986fb
Park640 <102134394+Park640@users.noreply.github.com>
2022-07-27 18:17:30 +0900

    Asset Add

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] Devil;
+    public GameObject[] Spear;
+    public GameObject[] Earth;
+    public GameObject[] Things;
+
+    private void Update()
+    {
+        
+    }
+}
