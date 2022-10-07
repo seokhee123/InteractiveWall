10a442d93b99a8c6ee41bcc18ebaab385b989816
seokhee123 <seokhee1203@naver.com>
2022-10-07 17:06:15 +0900

    DrawLine

---
+++ Assets/Merge.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item
+{
+    public int itemNum;¡¡
+    public Sprite itemlmg;
+
+}
+public class Merge : MonoBehaviour
+{
+    public List<Item> itemdata = new List<Item>();
+    //public 
+}
